09be1d22889bb7cd3666e69b2909ae0b7638c0c8
Aidan Bane <aidanfbane@gmail.com>
2017-08-17 17:49:34 -0700

    (feat) Updated Title on /

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div className='App'>
         <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Redux-todos!</h2>
         </div>
         <Router>
           <div className='Todo-app'>
